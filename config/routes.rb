50576a1c5149215f1026517642b25a4cd690f844
WinCodeX <glenwinterg970@gmail.com>
2023-02-21 17:20:02 +0300

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   root "home#index"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
